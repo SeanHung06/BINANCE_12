84fbd180587b5ed409e553a97cb352f6d7709be7
seanhung06 <u8351574@gmail.com>
2019-12-19 01:50:45 +0800

    modified:   binance_ETHUSDT_data.csv 	modified:   binance_ETHUSDT_data.xlsx 	modified:   email_send.py

--- email_send.py
+++ email_send.py
@@ -1,22 +1,39 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import csv
+fileToSend = "binance_ETHUSDT_data.csv"
+
 from_address = "u8351574@gmail.com"
 #to_address = "clarehuang951102@gmail.com"
 to_address = "u8351574@gmail.com"
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
-msg['Subject'] = "Test email"
+
 msg['From'] = from_address
 msg['To'] = to_address
-# Create the message (HTML).
+# Create the message (CSV).
+
+# Open the file csv and input the content in the loop 
+f = open('binance_ETHUSDT_data.csv')
+content = 'Hi'
+content += '\n\nThis is an automated message.\n\n'
+reader = csv.reader(f)
+for row in reader:
+  content = content + str(row)+'\n'
+  
+
+content += '\nRegards Sean'
+msg = MIMEText(content)
+msg['Subject'] = "Test email"
 html = """\
-We are sending an email using Python and Gmail Hi Abao I LOVE 這是用程式寄信的XD
+We are sending an email using Python and Gmail
  """
 # Record the MIME type - text/html.
 part1 = MIMEText(html, 'html')
 # Attach parts into message container
-msg.attach(part1)
+#msg.attach(part1)
+
 # Credentials
 username = 'u8351574@gmail.com'  
 password = 'qknvwdlikvbozwap'  
