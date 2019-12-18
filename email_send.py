aa7f6e65877936e88b65d059969ffa9fd34e1c81
seanhung06 <u8351574@gmail.com>
2019-12-19 00:53:32 +0800

    new file:   __pycache__/email.cpython-38.pyc 	renamed:    email.py -> email_send.py

--- email_send.py
+++ email_send.py
@@ -1,8 +1,9 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from_address = "from_email@gmail.com"
-to_address = "to_email@gmail.com"
+from_address = "u8351574@gmail.com"
+#to_address = "clarehuang951102@gmail.com"
+to_address = "u8351574@gmail.com"
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "Test email"
@@ -10,15 +11,15 @@ msg['From'] = from_address
 msg['To'] = to_address
 # Create the message (HTML).
 html = """\
-We are sending an email using Python and Gmail, how fun! We can fill this with html, and gmail supports a decent range of css style attributes too - https://developers.google.com/gmail/design/css#example.
+We are sending an email using Python and Gmail Hi Abao I LOVE 這是用程式寄信的XD
  """
 # Record the MIME type - text/html.
 part1 = MIMEText(html, 'html')
 # Attach parts into message container
 msg.attach(part1)
 # Credentials
-username = 'example_email@gmail.com'  
-password = 'your_password'  
+username = 'u8351574@gmail.com'  
+password = 'qknvwdlikvbozwap'  
 # Sending the email
 ## note - this smtp config worked for me, I found it googling around, you may have to tweak the # (587) to get yours to work
 server = smtplib.SMTP('smtp.gmail.com', 587) 
